b7ae813fd135eb7322bc8f29d7c5ef4666be8d95
LucasCA-Git <lucascardosoalecrim99@gmail>
2025-04-21 22:26:42 -0300

    retirando a previsao

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src.carregamento_dados import carregar_dados
 from src.analise_geral import resumo_geral
 from src.analise_tipo import tipos_de_chamado, chamados_sem_tipo
 from src.analise_temporal import chamados_por_temporalidade
-from src.previsao_chamados import previsao_volume_chamados
 
 def main():
     caminho_pasta = 'dados_tiflux'  # Altere esse nome se sua pasta tiver outro nome
@@ -20,8 +19,6 @@ def main():
     # Nova análise: chamados por semana dentro de cada mês
     chamados_por_temporalidade(df)
 
-    # Previsão de volume de chamados
-    previsao_volume_chamados(df)
 
 if __name__ == "__main__":
     main()
